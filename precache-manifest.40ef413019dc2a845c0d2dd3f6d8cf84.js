82ff9e8e43fd59cd1b0767a1b788db13a841aabf
Justin1095 <jseda3237@gmail.com>
2020-06-21 14:58:41 -0400

    Updates

---
+++ precache-manifest.40ef413019dc2a845c0d2dd3f6d8cf84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe92b661e874caa31a19cb18ad62c21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2fb3f7c33925e4388fdd",
+    "url": "/monsters-rolodex/static/css/main.944f021b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb3f7c33925e4388fdd",
+    "url": "/monsters-rolodex/static/js/main.4eb166dd.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
